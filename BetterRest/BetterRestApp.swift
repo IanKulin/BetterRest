0c67d1030216160c264f060ed95c93a14a62ff00
Ian Bailey <IanKulin@kulin.com.au>
2022-09-13 16:54:00 +0800

    Initial

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ian Bailey on 10/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
